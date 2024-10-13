b0d72cad61de4bd5f0cc913681604dba65f5b06d
DimaUpGrade <dima2002bord@yandex.ru>
2024-10-13 13:48:15 +0300

    fix trouble with quotes

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from config import TOKEN
 from SI import mbar_to_mm_hg
 from weather import current_weather, daily_weather, today_weather
 
-# print(TOKEN)
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -18,10 +17,10 @@ def send_welcome(message):
 def send_welcome(message):
     if data := current_weather(message.text):
         kb = types.InlineKeyboardMarkup(row_width=2)
-        today_forecast_btn = types.InlineKeyboardButton(text="Прогноз на день", callback_data=f"today_forecast&{data["city_name"]}&{data["country_code"]}&{message.chat.id}")
-        daily_forecast_btn = types.InlineKeyboardButton(text="Прогноз на 16 дней", callback_data=f"daily_forecast&{data["city_name"]}&{data["country_code"]}&{message.chat.id}")
+        today_forecast_btn = types.InlineKeyboardButton(text="Прогноз на день", callback_data=f"today_forecast&{data['city_name']}&{data['country_code']}&{message.chat.id}")
+        daily_forecast_btn = types.InlineKeyboardButton(text="Прогноз на 16 дней", callback_data=f"daily_forecast&{data['city_name']}&{data['country_code']}&{message.chat.id}")
         kb.add(today_forecast_btn, daily_forecast_btn)
-        bot.send_message(message.chat.id, f"<b>{data["city_name"]} [{data["country_code"]}]</b>, сейчас:\n\n<b>Температура:</b> {data["temp"]} °C\n<b>Влажность:</b> {data["rh"]}%\n<b>Давление:</b> {mbar_to_mm_hg(data["pres"])} мм. рт. ст.", parse_mode='HTML',
+        bot.send_message(message.chat.id, f"<b>{data['city_name']} [{data['country_code']}]</b>, сейчас:\n\n<b>Температура:</b> {data['temp']} °C\n<b>Влажность:</b> {data['rh']}%\n<b>Давление:</b> {mbar_to_mm_hg(data['pres'])} мм. рт. ст.", parse_mode='HTML',
                          reply_markup=kb)
     else:
         bot.send_message(message.chat.id, "Что-то пошло не так...")
@@ -32,20 +31,17 @@ def check_callback_data(call):
     if call.data:
         call_data = call.data.split("&")
         if call_data[0] == "daily_forecast":
-            # print(call_data)
             if data := daily_weather(call_data[1]):
                 bot.send_message(call_data[3], f"<b>Погода в городе {call_data[1]} [{call_data[2]}] на 16 дней:</b>", parse_mode='HTML')
                 for daily in data:
-                    # print(daily)
-                    bot.send_message(call_data[3], f"<b><i>{call_data[1]} [{call_data[2]}], {daily["datetime"]}</i></b>\n\n<b>Средняя температура:</b> {daily["temp"]} °C\n<b>Максимальная температура:</b> {daily["max_temp"]} °C\n<b>Минимальная температура:</b> {daily["min_temp"]} °C\n<b>Вероятность осадков:</b> {daily["pop"]}%\n<b>Ветер:</b> {daily["wind_cdir"]}, {daily["wind_spd"]} м/с\n<b>Давление:</b> {mbar_to_mm_hg(daily["pres"])} мм. рт. ст.\n<b>Влажность:</b> {daily["rh"]}%", parse_mode='HTML')
+                    bot.send_message(call_data[3], f"<b><i>{call_data[1]} [{call_data[2]}], {daily['datetime']}</i></b>\n\n<b>Средняя температура:</b> {daily['temp']} °C\n<b>Максимальная температура:</b> {daily['max_temp']} °C\n<b>Минимальная температура:</b> {daily['min_temp']} °C\n<b>Вероятность осадков:</b> {daily['pop']}%\n<b>Ветер:</b> {daily['wind_cdir']}, {daily['wind_spd']} м/с\n<b>Давление:</b> {mbar_to_mm_hg(daily['pres'])} мм. рт. ст.\n<b>Влажность:</b> {daily['rh']}%", parse_mode='HTML')
             else:
                 bot.send_message(call_data[3], "Что-то пошло не так...")
         elif call_data[0] == "today_forecast":
             if data := today_weather(call_data[1]):
                 bot.send_message(call_data[3], f"<b>Погода в городе {call_data[1]} [{call_data[2]}] на 24 часа:</b>", parse_mode='HTML')
                 for hour in range(24):
-                    # data[hour]
-                    bot.send_message(call_data[3], f"<b><i>{call_data[1]} [{call_data[2]}], {', '.join(data[hour]["timestamp_local"].split("T"))} (местное)</i></b>\n\n<b>Температура:</b> {data[hour]["temp"]} °C\n<b>Ощущается:</b> {data[hour]["app_temp"]} °C\n<b>Вероятность осадков:</b> {data[hour]["pop"]}%\n<b>Облачность:</b> {data[hour]["clouds"]}%\n<b>Ветер:</b> {data[hour]["wind_cdir"]}, {data[hour]["wind_spd"]} м/с\n<b>Давление:</b> {mbar_to_mm_hg(data[hour]["pres"])} мм. рт. ст.\n<b>Влажность:</b> {data[hour]["rh"]}%", parse_mode='HTML')
+                    bot.send_message(call_data[3], f"<b><i>{call_data[1]} [{call_data[2]}], {', '.join(data[hour]['timestamp_local'].split('T'))} (местное)</i></b>\n\n<b>Температура:</b> {data[hour]['temp']} °C\n<b>Ощущается:</b> {data[hour]['app_temp']} °C\n<b>Вероятность осадков:</b> {data[hour]['pop']}%\n<b>Облачность:</b> {data[hour]['clouds']}%\n<b>Ветер:</b> {data[hour]['wind_cdir']}, {data[hour]['wind_spd']} м/с\n<b>Давление:</b> {mbar_to_mm_hg(data[hour]['pres'])} мм. рт. ст.\n<b>Влажность:</b> {data[hour]['rh']}%", parse_mode='HTML')
 
 
 bot.infinity_polling()
